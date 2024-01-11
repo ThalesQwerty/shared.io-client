30f61630a90ad2baed9e593cbaef159d6885f1c9
Thales B. Rodrigues <thalesbrito05@gmail.com>
2024-01-11 15:00:26 -0300

    :sparkles: Create Promises for createEntity and joinChannel methods

--- test/manual/index.js
+++ test/manual/index.js
@@ -1,34 +1,7 @@
 import SharedIO from "../../lib/index.js";
 
-class Player extends SharedIO.Entity {
-    name = "Thales";
-    x = 0;
-    y = 0;
-
-    constructor(channel, name) {
-        super(channel);
-
-        this.name = name;
-    }
-}
-
 const client = new SharedIO.Client("ws://localhost:3000");
 client.connect();
 
-client.on("connect", () => {
-    const channel = new SharedIO.Channel(client, "wololo");
-    channel.join();
-
-    window["channel"] = channel;
-
-    channel.on("join", () => {
-        console.log("JOINED", channel.id);
-    });
-
-    channel.on("createEntity", (event) => {
-        console.log("NEW ENTITY", event.entity);
-    });
-});
-
-window["Entity"] = SharedIO.Entity;
-window["Player"] = Player;
+window["client"] = client;
+window["SharedIO"] = SharedIO;

6e9559426e88fcf8182dc71f44e4898bef5c5488
Thales B. Rodrigues <thalesbrito05@gmail.com>
2024-01-13 22:28:17 -0300

    :test_tube: Improve demo usability

--- test/manual/index.js
+++ test/manual/index.js
@@ -7,79 +7,102 @@ window["SharedIO"] = SharedIO;
 
 await client.connect();
 
-const channelName = new URLSearchParams(window.location.search).get("channel") ?? "main";
-const channel = await client.joinChannel(channelName);
+async function joinChannel(channelName) {
+    document.querySelectorAll(".channel_name").forEach(span => span.innerHTML = channelName);
 
-console.log("Joined channel", channelName);
+    document.querySelector("#join_channel").style.display = "none";
+    document.querySelector("#loading_channel").style.display = "block";
+    document.querySelector("#leave_channel").style.display = "none";
 
-channel.on("createEntity", ({ entity }) => {
-    console.log("created", entity);
-    createSquare(entity);
-});
+    const channel = await client.joinChannel(channelName);
 
-/**
- * @param {Entity} entity 
- */
-function createSquare(entity) {
-    const square = document.createElement("div");
-    square.style.width = square.style.height = "32px";
-    square.style.border = `solid 2px black`;
-    square.style.position = "absolute";
-    square.style.background = entity.color;
-    square.style.top = entity.position.y + "px";
-    square.style.left = entity.position.x + "px";
+    document.querySelector("#loading_channel").style.display = "none";
+    document.querySelector("#leave_channel").style.display = "block";
 
-    document.body.appendChild(square);
+    console.log("Joined channel", channelName);
 
-    entity._square = square;
-
-    entity.on("delete", () => {
-        square.remove();
+    channel.on("createEntity", ({ entity }) => {
+        console.log("created", entity);
+        createSquare(entity);
     });
 
-    if (entity.owned) {
-        window.addEventListener("keydown", event => {
-            entity._controls[event.key] = true;
+    /**
+     * @param {Entity} entity 
+     */
+    function createSquare(entity) {
+        const square = document.createElement("div");
+        square.style.width = square.style.height = "32px";
+        square.style.border = `solid 2px black`;
+        square.style.position = "absolute";
+        square.style.background = entity.color;
+        square.style.top = entity.position.y + "px";
+        square.style.left = entity.position.x + "px";
+        square.style.zIndex = -1;
+        square.style.pointerEvents = "none";
+
+        document.querySelector("#squares").appendChild(square);
+
+        entity._square = square;
+
+        entity.on("delete", () => {
+            square.remove();
         });
 
-        window.addEventListener("keyup", event => {
-            entity._controls[event.key] = false;
-        });
-    }
-}
+        if (entity.owned) {
+            window.addEventListener("keydown", event => {
+                entity._controls[event.key] = true;
+            });
 
-const squareSpeed = 128;
-let lastUpdate = Date.now();
-
-function animate() {
-    const deltaTime = (Date.now() - lastUpdate) / 1000;
-    const displacement = squareSpeed * deltaTime;
-    lastUpdate = Date.now();
-
-    for (const entity of channel.entities) {
-        if (!entity._square) return;
-        entity._square.style.top = entity.position.y + "px";
-        entity._square.style.left = entity.position.x + "px";
-
-        if (entity.owned) {       
-            if (entity._controls.ArrowRight) entity.position.x += displacement;
-            if (entity._controls.ArrowLeft) entity.position.x -= displacement;
-
-            if (entity._controls.ArrowDown) entity.position.y += displacement;
-            if (entity._controls.ArrowUp) entity.position.y -= displacement;
+            window.addEventListener("keyup", event => {
+                entity._controls[event.key] = false;
+            });
         }
     }
 
-    requestAnimationFrame(animate);
+    const squareSpeed = 128;
+    let lastUpdate = Date.now();
+
+    function animate() {
+        const deltaTime = (Date.now() - lastUpdate) / 1000;
+        const displacement = squareSpeed * deltaTime;
+        lastUpdate = Date.now();
+
+        for (const entity of channel.entities) {
+            if (!entity._square) return;
+            entity._square.style.top = entity.position.y + "px";
+            entity._square.style.left = entity.position.x + "px";
+
+            if (entity.owned) {       
+                if (entity._controls.ArrowRight) entity.position.x += displacement;
+                if (entity._controls.ArrowLeft) entity.position.x -= displacement;
+
+                if (entity._controls.ArrowDown) entity.position.y += displacement;
+                if (entity._controls.ArrowUp) entity.position.y -= displacement;
+            }
+        }
+
+        requestAnimationFrame(animate);
+    }
+
+    await channel.createEntity({
+        color: "#" + Math.floor(Math.random() * Math.pow(16, 6)).toString(16).padStart(6, "0"),
+        position: {
+            x: Math.random() * 256,
+            y: Math.random() * 256
+        },
+        _controls: {}
+    });
+
+    window["leaveChannel"] = function() {
+        channel.leave();
+
+        document.querySelector("#join_channel").style.display = "block";
+        document.querySelector("#loading_channel").style.display = "none";
+        document.querySelector("#leave_channel").style.display = "none";
+    }
+
+    animate();
 }
 
-await channel.createEntity({
-    color: "#" + Math.floor(Math.random() * Math.pow(16, 6)).toString(16).padStart(6, "0"),
-    position: {
-        x: Math.random() * 256,
-        y: Math.random() * 256
-    },
-    _controls: {}
-});
-
-animate();
+window["joinChannel"] = joinChannel;
+window["leaveChannel"] = function() {};

b6cb71717930c78437795ab66ebba5baea0a502c
Thales B. Rodrigues <thalesbrito05@gmail.com>
2024-01-23 17:04:30 -0300

    :test_tube: Create automated tests for Entity

--- test/Client.test.js
+++ test/Client.test.js
@@ -1,5 +1,6 @@
 import { Channel } from "../lib/models/Channel";
 import { Client } from "../lib/models/Client";
+import { Entity } from "../lib/models/Entity";
 import { delay } from "./helpers/delay";
 import { WS as WebSocketServer } from "jest-websocket-mock";
 
@@ -79,50 +80,174 @@ describe("Client", () => {
     });
 
     describe("Outputs", () => {
-        test("Join and leave", async () => {
+        describe("Channels", () => {
+            test("Join and leave", async () => {
+                /**
+                 * @type {Channel}
+                 */
+                let channel;
+                let hasLeft = false;
+
+                await client.connect();
+                await server.connected;
+
+                client.on("joinChannel", event => {
+                    channel = event.channel;
+                });
+
+                client.on("leaveChannel", () => {
+                    hasLeft = true;
+                });
+
+                server.send(JSON.stringify({
+                    action: "join",
+                    channelId: "test"
+                }));
+
+                await delay(50);
+
+                expect(client.channels).toHaveLength(1);
+                expect(client.channels).toContain(channel);
+                expect(channel).toBeDefined();
+                expect(channel.id).toBe("test");
+                expect(channel.client).toBe(client);
+                expect(hasLeft).toBe(false);
+
+                server.send(JSON.stringify({
+                    action: "leave",
+                    channelId: "test"
+                }));
+
+                await delay(50);
+
+                expect(client.channels).toHaveLength(0);
+                expect(client.channels).not.toContain(channel);
+                expect(channel.id).toBe("test");
+                expect(channel.client).toBe(client);
+                expect(hasLeft).toBe(true);
+            });
+        });
+
+        describe("Entities", () => {
             /**
              * @type {Channel}
              */
             let channel;
-            let hasLeft = false;
 
-            await client.connect();
-            await server.connected;
+            beforeEach(async () => {
+                await client.connect();
+                await server.connected;
 
-            client.on("joinChannel", event => {
-                channel = event.channel;
+                server.send(JSON.stringify({
+                    action: "join",
+                    channelId: "test"
+                }));
+
+                await delay(50);
+
+                expect(client.channels).toHaveLength(1);
+
+                channel = client.channels[0];
+                expect(channel).toBeDefined();
+                expect(channel.entities).toHaveLength(0);
             });
 
-            client.on("leaveChannel", () => {
-                hasLeft = true;
+            test("Create and update non-owned entity", async () => {
+                /**
+                 * @type {Entity}
+                 */
+                let entity;
+
+                channel.on("createEntity", event => {
+                    entity = event.entity;
+                })
+
+                server.send(JSON.stringify({
+                    action: "create",
+                    channelId: "test",
+                    params: {
+                        entityId: "entity",
+                        values: {
+                            a: 1,
+                            b: 2
+                        }
+                    }
+                }));
+
+                await delay(50);
+
+                expect(channel.entities).toHaveLength(1);
+                expect(entity).toBeDefined();
+                expect(channel.entities).toContain(entity);
+                expect(entity.owned).toBe(false);
+                expect(entity.a).toBe(1);
+                expect(entity.b).toBe(2);
+
+                server.send(JSON.stringify({
+                    action: "update",
+                    channelId: "test",
+                    params: {
+                        entityId: "entity",
+                        values: {
+                            a: 3
+                        }
+                    }
+                }));
+
+                await delay(50);
+
+                expect(channel.entities).toHaveLength(1);
+                expect(channel.entities).toContain(entity);
+                expect(entity.owned).toBe(false);
+                expect(entity.a).toBe(3);
+                expect(entity.b).toBe(2);
             });
 
-            server.send(JSON.stringify({
-                action: "join",
-                channelId: "test"
-            }));
+            test("Create owned entity", async () => {
+                /**
+                 * @type {Entity}
+                 */
+                let createdEntity;
 
-            await delay(50);
+                channel.on("createEntity", event => {
+                    createdEntity = event.entity;
+                })
 
-            expect(client.channels).toHaveLength(1);
-            expect(client.channels).toContain(channel);
-            expect(channel).toBeDefined();
-            expect(channel.id).toBe("test");
-            expect(channel.client).toBe(client);
-            expect(hasLeft).toBe(false);
+                channel.createEntity({
+                    a: 1,
+                    b: 2
+                }); 
 
-            server.send(JSON.stringify({
-                action: "leave",
-                channelId: "test"
-            }));
+                expect(channel.entities).toHaveLength(1);
+                
+                const entity = channel.entities[0];
+                expect(entity).toBeDefined();
+                expect(entity.owned).toBe(true);
+                expect(entity.active).toBe(false);
+                expect(entity.a).toBe(1);
+                expect(entity.b).toBe(2);
+                expect(createdEntity).toBeUndefined();
 
-            await delay(50);
+                server.send(JSON.stringify({
+                    action: "create",
+                    channelId: "test",
+                    params: {
+                        entityId: entity.id,
+                        values: {
+                            a: 1,
+                            b: 2
+                        }
+                    }
+                }));
 
-            expect(client.channels).toHaveLength(0);
-            expect(client.channels).not.toContain(channel);
-            expect(channel.id).toBe("test");
-            expect(channel.client).toBe(client);
-            expect(hasLeft).toBe(true);
+                await delay(50);
+
+                expect(entity.owned).toBe(true);
+                expect(entity.active).toBe(true);
+                expect(entity.a).toBe(1);
+                expect(entity.b).toBe(2);
+                expect(createdEntity).toBe(entity);
+            });
         });
     });
 });
